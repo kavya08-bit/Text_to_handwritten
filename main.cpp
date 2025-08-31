4890b50e40a2d936dff7577ce24bcca03007095c
kavya08-bit <kavyapalsingh44@gmail.com>
2025-08-31 11:28:53 +0530

    code done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
-#include<iostream>
+#include <opencv2/opencv.hpp>
+#include <iostream>
 
-int main(){
-    
+int main() {
+
+    int width = 800, height = 200;
+    cv::Mat image(height, width, CV_8UC3, cv::Scalar(255, 255, 255));
+
+
+    std::string text = "Hello, Kavya! This is handwritten-style text.";
+
+
+    int fontFace = cv::FONT_HERSHEY_SCRIPT_SIMPLEX; 
+    double fontScale = 1.5;
+    int thickness = 2;
+
+
+    cv::Point textOrg(50, 100);
+    cv::putText(image, text, textOrg, fontFace, fontScale, cv::Scalar(0, 0, 0), thickness);
+
+    cv::imshow("Handwritten Style", image);
+    cv::waitKey(0);
+
+
+    cv::imwrite("handwritten_output.png", image);
+
+    return 0;
 }
